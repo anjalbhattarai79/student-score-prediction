b34e6f4967dea9338265fb7be04a811012367939
anjalbhattarai79 <anjalbhattarai79@gmail.com>
2025-07-25 19:49:35 +0545

    update main.py for allow_origins to avaoid path parameter

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5500", 
                    "http://127.0.0.1:5500",
-                   "https://anjalbhattarai79.github.io/student-score-prediction/"], 
+                   "https://anjalbhattarai79.github.io"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
