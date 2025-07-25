cbc40774b8b0fbb65d4f5216934998a454a61843
anjalbhattarai79 <anjalbhattarai79@gmail.com>
2025-07-25 19:43:25 +0545

    update main.py --> allow_origins

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5500", 
                    "http://127.0.0.1:5500",
-                   "https://student-score-prediction-br3c.onrender.com"], 
+                   "https://anjalbhattarai79.github.io/student-score-prediction/"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
